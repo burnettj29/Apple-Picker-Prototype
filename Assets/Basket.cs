4e90f40374f5373ed19f1326ed190347fdb1a6e7
burnettj29 <48725610+burnettj29@users.noreply.github.com>
2019-03-28 11:01:32 -0400

    Added Score Keeping

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour {
 
+	public Text scoreGT;
+
 	// Use this for initialization
 	void Start () {
+		GameObject scoreGO = GameObject.Find ("ScoreCounter");
+		scoreGT = scoreGO.GetComponent<Text> ();
+		scoreGT.text = "0";
 		
 	}
 	
@@ -27,5 +33,9 @@ public class Basket : MonoBehaviour {
 		GameObject collidedWith = coll.gameObject;
 		if (collidedWith.tag == "Apple") 
 			Destroy (collidedWith);
+
+		int score = int.Parse (scoreGT.text);
+		score += 100;
+		scoreGT.text = score.ToString ();
 	}
 }
